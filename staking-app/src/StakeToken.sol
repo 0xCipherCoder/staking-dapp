6a7c3da527be78dd3f1b39e1bea2d4fce9acad3d
0xCipherCoder <ciphercoder0x@gmail.com>
2024-06-10 10:18:04 +0530

    Added contract changes

--- staking-app/src/StakeToken.sol
+++ staking-app/src/StakeToken.sol
@@ -1,11 +1,11 @@
 // contracts/GLDToken.sol
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.0;
 
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract StakeToken is ERC20 {
-    constructor(uint256 initialSupply) ERC20("Gold", "GLD") {
-        _mint(msg.sender, initialSupply);
+    constructor(uint256 initialSupply) ERC20("StakeToken", "STK") {
+        _mint(msg.sender, initialSupply*10**18);
     }
 }
